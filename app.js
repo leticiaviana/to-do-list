0ac0e273f8b225eebc75af8c5cbc4537bb3fa212
leticia_paixao <leticia.paixao@informata.com.br>
2023-05-08 17:27:26 -0300

    :hammer: add clear function

--- app.js
+++ app.js
@@ -12,18 +12,73 @@ let editID = '';
 
 form.addEventListener('submit', addItem);
 
+clearBtn.addEventListener('click', clearItems);
+
 
 function addItem(e){
     e.preventDefault();
     const value = grocery.value;
     const id = new Date().getTime().toString();
     if(value && !editFlag){
-        console.log('add item to the list');
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+        <div class="btn-container">
+            <button class="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button class="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>`;
+        list.appendChild(element);
+
+        displayAlert('item added to the list', 'success');
+        container.classList.add('show-container');
+
+        addToLocalStorage(id, value);
+        setBackToDefault();
     }
     else if(value && editFlag){
         console.log('editing');
     } else{
-        alert.textContent = 'empty value';
-        alert.classList.add('alert-danger');
+       displayAlert('please enter value', 'danger')
     }
+}
+
+function displayAlert(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    setTimeout(() =>{
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    }, 1000);
+}
+
+function setBackToDefault(){
+    grocery.value = '';
+    editFlag = false;
+    editID = '';
+    submitBtn.textContent = 'submit';
+}
+
+function clearItems(){
+    const items = document.querySelectorAll('.grocery-item');
+    if(items.length > 0){
+        items.forEach((item) =>{
+            list.removeChild(item);
+        });
+    }
+    container.classList.remove('show-container');
+    displayAlert('empty list', 'danger');
+    // localStorage,removeItem('list');
+    setBackToDefault();
+}
+
+function addToLocalStorage(id, value){
+    console.log('added to local storage');
 }
